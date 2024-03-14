a9c1b80a9b0401b69a2f082ffbb9dc228681d11a
rajeshKumarbit <banyalrajesh20@gmail.comgit config --global user.namegit config --global user.email banyalrajesh20@gmail.com>
2024-03-14 19:31:37 +0530

    build app.py and precict pipeline

--- src/utils.py
+++ src/utils.py
@@ -52,3 +52,12 @@ def evaluate_models(X_train, X_test, y_train, y_test, models, param):
         raise CustomException(e, sys)
         
         
+
+
+#load_object for loading the model file
+def load_object(filepath):
+    try:
+        with open(filepath, 'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
