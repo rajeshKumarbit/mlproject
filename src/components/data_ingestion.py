b6be8ce3dcc4d55f0fc2890ea744de306015fe59
rajeshKumarbit <banyalrajesh20@gmail.comgit config --global user.namegit config --global user.email banyalrajesh20@gmail.com>
2024-03-14 15:53:52 +0530

    model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,8 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer, ModelTrainingConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -49,6 +51,11 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
+
+
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_training(train_arr, test_arr))
         
